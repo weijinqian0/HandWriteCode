0df32f66affb5521b64d0d787cc6946a734c9cb6
weijinqian <1184188277@qq.com>
2020-03-14 18:43:58 +0800

    剑指offer 矩阵中的路径

---
+++ src/com/weijinqian/Solution.java
@@ -0,0 +1,12 @@
+package com.weijinqian;
+
+public class Solution {
+
+    public static void print(int value) {
+        System.out.println(value);
+    }
+
+    public static void printBoolean(boolean value) {
+        System.out.println(value);
+    }
+}
