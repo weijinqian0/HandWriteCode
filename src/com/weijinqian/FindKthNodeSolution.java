d00e25bf5656818593d1915cc876828954b18242
weijinqian <1184188277@qq.com>
2020-03-28 17:53:46 +0800

    剑指offer 链表中倒数第k个节点

--- src/com/weijinqian/FindKthNodeSolution.java
+++ src/com/weijinqian/FindKthNodeSolution.java
@@ -1,6 +1,13 @@
 package com.weijinqian;
 
 public class FindKthNodeSolution {
+    /**
+     * 通过两个指针间隔k-1个距离来做
+     *
+     * @param head
+     * @param k
+     * @return
+     */
     public ListNode FindKthToTail(ListNode head, int k) {
         if (head == null) {
             return null;
@@ -25,5 +32,33 @@ public class FindKthNodeSolution {
             tmp = tmp.next;
         }
         return res;
+    }
+
+
+    private int curIdx = 0;
+    private ListNode res = null;
+
+    /**
+     * 通过递归的方式来做，相当于后序遍历
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public void FindKthToTail1(ListNode head, int k) {
+        if (head == null) {
+            return;
+        }
+        if (k <= 0) {
+            return;
+        }
+
+        if (curIdx == k) {
+            res = head;
+            return;
+        }
+        FindKthToTail1(head.next, k);
+        curIdx++;
+
     }
 }
