9e67d979dbcfb37931e96476499d64a7de9e1211
weijinqian <1184188277@qq.com>
2020-04-06 17:12:04 +0800

    剑指offer 二叉树的深度

---
+++ src/com/weijinqian/TreeDepthSolution.java
@@ -0,0 +1,11 @@
+package com.weijinqian;
+
+public class TreeDepthSolution {
+    public int TreeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
+    }
+
+}
