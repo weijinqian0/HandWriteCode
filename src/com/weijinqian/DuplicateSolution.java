ec25c61fb133cf33451f41870324b0fcbe945890
weijinqian <1184188277@qq.com>
2020-04-11 17:43:04 +0800

    剑指offer 数组中重复的数字

---
+++ src/com/weijinqian/DuplicateSolution.java
@@ -0,0 +1,27 @@
+package com.weijinqian;
+
+public class DuplicateSolution {
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        if (numbers == null || numbers.length == 0) {
+            return false;
+        }
+
+        int index = 0;
+        while (index < length) {
+            if (index == numbers[index]) {
+                index++;
+            } else {
+                int tmp = numbers[index];
+                // 要去的位置和该位置存的值比较一下，是否相等，相等则重复，不等就交换
+                if (tmp == numbers[tmp]) {
+                    duplication[0] = tmp;
+                    return true;
+                } else {
+                    numbers[index] = numbers[tmp];
+                    numbers[tmp] = tmp;
+                }
+            }
+        }
+        return false;
+    }
+}
